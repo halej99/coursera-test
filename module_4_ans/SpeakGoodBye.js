ff9df5c7768a4170245819da24861d008af1b161
halej99 <halej99@yahoo.com>
2024-08-06 13:12:01 -0700

    Update SpeakGoodBye.js

--- module_4_ans/SpeakGoodBye.js
+++ module_4_ans/SpeakGoodBye.js
@@ -24,5 +24,5 @@ byeSpeaker.speak= function speak(name) {
 // STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
 // 'byeSpeaker' on the global scope as well.
 // xxxx.xxxx = byeSpeaker;
-window.byeSpeaker =byeSpeaker;
+window.byeSpeaker = byeSpeaker;
 })();
